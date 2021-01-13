b267bdc502f049518d26fb0fdad64a617fdb354c
Aadomas <okamjoreikia@gmail.com>
2021-01-13 20:35:12 +0200

    Js intro + namu darbai

---
+++ js/namu.darbai.js
@@ -0,0 +1,68 @@
+const pirmas = 1;
+console.log(pirmas);
+
+const antras = 2;
+console.log(antras);
+
+const trecias = 3;
+console.log(trecias);
+
+console.log('-------------------------------');
+
+const vienas = 'bananas';
+console.log(vienas);
+
+const du = 'apelsinas';
+console.log(du);
+
+const trys = 'agurkas';
+console.log(trys);
+
+console.log('-------------------------------');
+
+const a = [1, 2, 3, 4, 5,];
+console.log(a);
+const b = [11, 22, 33, 44, 55,];
+console.log(b);
+const c = [111, 222, 333, 444, 555,];
+console.log(c);
+
+console.log('-------------------------------');
+
+const x = ['vienas', 'du', 'trys', 'keturi', 'penki'];
+console.log(x);
+const y = ['vienas', 'du', 'trys', 'keturi', 'penki'];
+console.log(y);
+const z = ['vienas', 'du', 'trys', 'keturi', 'penki'];
+console.log(z);
+
+console.log('-------------------------------');
+
+const suma = pirmas + antras + trecias;
+console.log(pirmas, '+', antras, '+', trecias, '=', suma);
+
+console.log('-------------------------------');
+
+const sujungimas = vienas + ' ' + du + ' ' + trys
+console.log(sujungimas);
+
+console.log('-------------------------------');
+
+const Arezultatas = a[0] - a[1] + a[2] - a[3] + a[4];
+console.log('a =', Arezultatas)
+const Brezultatas = b[0] - b[1] + b[2] - b[3] + b[4];
+console.log('b =', Brezultatas);
+const Crezultatas = c[0] - c[1] + c[2] - c[3] + c[4];
+console.log('c =', Crezultatas);
+
+console.log('-------------------------------');
+
+console.log(z + ',' + y + ' ' + x);
+
+console.log('-------------------------------');
+
+
+
+
+
+
